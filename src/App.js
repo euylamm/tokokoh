2abfa70355238db112678b0d37dafa368092a3cc
Alam <euylamm@gmail.com>
2024-07-11 13:35:43 +0700

    add category

--- src/App.js
+++ src/App.js
@@ -2,42 +2,50 @@ import logo from './logo.svg';
 import './App.css';
 
 import React from 'react';
+import Category from './components/Category';
 
 function App() {
-  const [results, setResults] = React.useState([]);
+    const [results, setResults] = React.useState([]);
 
-  React.useEffect(() => {
-    fetch("http://localhost:3001/categories")
-      .then(response => response.json())
-      .then(data => {
-        console.log(data);
-        setResults(data);
-      })
-  }, [])
+    React.useEffect(() => {
+        fetch("http://localhost:3001/categories")
+            .then(response => response.json())
+            .then(data => {
+                console.log(data);
+                setResults(data);
+            })
+    }, [])
 
-  return (
-    <>
-    <header>TOKOKOH</header>
-    
-    <section>
-      <nav>
-        {
-          results.map(d => (
-          <div key={d.id}>{d.title}</div>
-        ))
+    const renderCategories = () => {
+        const categories = [];
+        for (let i = 0; i < results.length; i++) {
+            categories.push(<Category key={results[i].id} id={results[i].id} title={results[i].title} />)
         }
-      </nav>
-      <article>
-        main area
-      </article>
-    </section>
 
-    <footer>
-      footer
-    </footer>
+        return categories;
+    }
 
-    </>
-  );
+    return (
+        <>
+            <header>TOKOKOH</header>
+
+            <section>
+                <nav>
+                    {
+                        results && renderCategories()
+                    }
+                </nav>
+                <article>
+                    main area
+                </article>
+            </section>
+
+            <footer>
+                footer
+            </footer>
+
+        </>
+    );
 }
 
 export default App;
